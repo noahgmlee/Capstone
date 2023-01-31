58e962d07736a589206e7fb07bf39f043bdc326a
noahgmlee <noah12lee@gmail.com>
2023-01-30 19:10:18 -0500

    structuring project

---
+++ src/main/main.ino
@@ -0,0 +1,15 @@
+
+////// LOCAL FILES USED ///////
+#include "bt_functions.h"
+
+////// GLOBAL VARIABLES ///////
+
+////// SETUP /////// 
+void setup() {
+
+}
+ 
+////// LOOP //////
+void loop() {
+
+}
